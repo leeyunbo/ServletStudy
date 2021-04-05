8addea5f492b5d85d2d9b91a5b5a5aa50d995133
leeyunbo <servers1@naver.com>
2021-04-05 21:08:17 +0900

    [개발] HttpServletRequest 객체 활용해보기

---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,24 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+    // url 호출시 가장 먼저 수행되는 메서드
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //START-LINE
+
+
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> System.out.println("headerName = " + headerName));
+
+
+    }
+}
