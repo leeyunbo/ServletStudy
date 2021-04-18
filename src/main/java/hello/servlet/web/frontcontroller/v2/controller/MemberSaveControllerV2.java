f14645addc1b0ad990216ab47ccb7dcf5a53efbd
leeyunbo <servers1@naver.com>
2021-04-18 19:39:44 +0900

    [개발] 프론트 컨트롤러에 viewResolver(논리 URL -> 물리 URL로 변경) 추가

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -17,7 +17,7 @@ public class MemberSaveControllerV2 implements ControllerV2 {
 
     @Override
     public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("MemberSaveServlet.service");
+        System.out.println("MemberSaveControllerV2.process");
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
